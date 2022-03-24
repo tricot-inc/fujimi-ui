b2e64a8d7b7708279b383b5fadbd107fb1bdabe8
Junya Kono <junya03dance@gmail.com>
2022-03-24 23:38:15 +0900

    fix: icons を support するコンポーネントを fujimi-ui-react から local に変更 (#38)

---
+++ packages/icons/src/support/Text/Text.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled, {FlattenSimpleInterpolation} from 'styled-components';
+import {
+  TextStyle,
+  Color,
+  TextAlign,
+  Typography,
+  LineHeight,
+  LetterSpacing,
+} from '@tricot-inc/fujimi-ui';
+
+const Wrapper = styled.span<{
+  block: boolean;
+  color: string;
+  align: TextAlign;
+  lineHeight?: typeof LineHeight[keyof typeof LineHeight];
+  letterSpacing?: typeof LetterSpacing[keyof typeof LetterSpacing];
+  bold: boolean;
+  typography: TypographyViewPort;
+}>`
+  ${TextStyle}
+`;
+
+interface TypographyViewPort {
+  mobile?: FlattenSimpleInterpolation;
+  tablet?: FlattenSimpleInterpolation;
+  desktop?: FlattenSimpleInterpolation;
+}
+
+export type Props = React.HTMLAttributes<HTMLElement> & {
+  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
+  block?: boolean;
+  bold?: boolean;
+  color?: string;
+  align?: TextAlign;
+  lineHeight?: typeof LineHeight[keyof typeof LineHeight];
+  letterSpacing?: typeof LetterSpacing[keyof typeof LetterSpacing];
+  typography?: TypographyViewPort;
+  children?: React.ReactNode;
+};
+
+export const Text: React.VFC<Props> = ({
+  block,
+  color,
+  align,
+  bold,
+  typography,
+  children,
+  ...rest
+}) => (
+  <Wrapper
+    {...rest}
+    block={block as boolean}
+    color={color}
+    align={align as TextAlign}
+    bold={bold as boolean}
+    typography={typography as TypographyViewPort}
+  >
+    {children}
+  </Wrapper>
+);
+
+Text.defaultProps = {
+  as: 'span',
+  block: false,
+  bold: false,
+  color: Color.GRAY_ALPHA.GRAY_90,
+  align: 'left',
+  typography: {mobile: Typography.SF_NORMAL_16},
+};
